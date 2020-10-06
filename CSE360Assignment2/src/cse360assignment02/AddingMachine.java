f967412b5f2ebfc0b6e5bf0c203297d333ca1c0d
coras <coras@DESKTOP-JEPHUBT>
2020-10-05 22:16:07 -0700

    Initial setup - part 1

--- CSE360Assignment2/src/cse360assignment02/AddingMachine.java
+++ CSE360Assignment2/src/cse360assignment02/AddingMachine.java
@@ -1,35 +1,26 @@
 package cse360assignment02;
 
 public class AddingMachine {
-	private int total;
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () {
+    return 0;
+  }
+  
+  public void add (int value) {
+  }
 
-	private String tostring;
+  public void subtract (int value) {
+  }
 
-	public AddingMachine() {
-		total = 0; // not needed - included for clarity
-		tostring = "0";
-	}
+  public String toString () {
+    return "";
+  }
 
-	public int getTotal() {
-		return total;
-	}
-
-	public void add(int value) {
-		total += value;
-		tostring = tostring + " + " + value;
-	}
-
-	public void subtract(int value) {
-		total -= value;
-		tostring = tostring + " - " + value;
-	}
-
-	public String toString() {
-		return tostring;
-	}
-
-	public void clear() {
-		tostring = "0";
-		total = 0;
-	}
+  public void clear() {
+  }
 }
